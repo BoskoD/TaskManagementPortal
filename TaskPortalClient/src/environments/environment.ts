6b21b3b200ed80a41ddcdb541a9d0167573e4102
Bosko Danilovic <boskodanilovic@outlook.com>
2021-02-27 22:57:34 +0100

    :rocket: Updated angular base url

--- TaskPortalClient/src/environments/environment.ts
+++ TaskPortalClient/src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false
 };
 
-export const baseUrl = `https://localhost:5001/api/`;
+export const baseUrl = `https://taskportalapi20210227224026.azurewebsites.net/api/`;
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
